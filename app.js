3ba800a51d4a4addf1999893c2b75ca15b6cb445
Maxim Muravyev <y89108850235@yandex.ru>
2022-08-29 02:34:47 +0300

    PW13

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/users');
-const cardRoutes = require('./routes/cards');
+const cardRouter = require('./routes/cards');
+const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
@@ -18,10 +17,11 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', userRoutes);
-app.use('/', cardRoutes);
+app.use('/', userRouter);
+app.use('/', cardRouter);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Страница не найдена!' });
