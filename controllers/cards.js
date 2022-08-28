d19b9f6c39c07b75dd1d1af9f34f0e048b0916fa
Maxim Muravyev <y89108850235@yandex.ru>
2022-08-29 00:28:52 +0300

    PW13

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,9 +1,9 @@
 const Cards = require('../models/cards');
 
-module.exports.getCard = (req, res) => {
-  Cards.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: 'Ошибка по-умолчанию', err }));
+module.exports.getCards = async (req, res) => {
+  await Cards.find({})
+    .then((cards) => res.send(cards))
+    .catch(() => res.status(500).send({ message: 'Ошибка по-умолчанию' }));
 };
 
 module.exports.createCard = (req, res) => {
@@ -21,12 +21,12 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCardById = (req, res) => {
   Cards.findByIdAndDelete(req.params.cardId)
-    .orFail(new Error('Не удалось найти карточку'))
+    .orFail(new Error('NotFound'))
     .then(() => res.send({ message: 'Удалено!' }))
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
-      } else if (err.statusCode === 404) {
+      } else if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Не удалось найти карточку' });
       } else {
         res.status(500).send({ message: 'Ошибка по-умолчанию' });
@@ -40,12 +40,12 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .orFail(new Error('Не удалось найти карточку'))
+    .orFail(new Error('NotFound'))
     .then((card) => res.send({ likes: card.likes }))
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
-      } else if (err.statusCode === 404) {
+      } else if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Не удалось найти карточку' });
       } else {
         res.status(500).send({ message: 'Ошибка по-умолчанию' });
@@ -64,7 +64,7 @@ module.exports.dislikeCard = (req, res) => {
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
-      } else if (err.statusCode === 404) {
+      } else if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Не удалось найти карточку' });
       } else {
         res.status(500).send({ message: 'Ошибка по-умолчанию' });
