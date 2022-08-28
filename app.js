8bd22ebe247e6094e4cef9dc84b099a2dcd0f46b
Maxim Muravyev <y89108850235@yandex.ru>
2022-08-28 16:34:48 +0300

    PW13

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use('/', userRoutes);
 app.use('/', cardRoutes);
