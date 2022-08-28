43470e42c64c0f944a280def1dc8beaa42b1752d
Maxim Muravyev <y89108850235@yandex.ru>
2022-08-29 01:44:26 +0300

    PW13

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const User = require('../models/users');
 
-module.exports.getUser = (req, res) => {
+module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch(() => res.status(500).send({ message: 'Ошибка' }));
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const {
   createUser,
   getUsers,
-  getUserById,
+  getUser,
   updateUser,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
 router.post('/users', createUser);
-router.get('/users/:userId', getUserById);
+router.get('/users/:userId', getUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
 
