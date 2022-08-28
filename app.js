e6adc442b36baaa7797cf50a88dae11b9e8d2b44
Maxim Muravyev <y89108850235@yandex.ru>
2022-08-29 02:24:42 +0300

    PW13

--- app.js
+++ app.js
@@ -5,27 +5,9 @@ const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62dd54d4aab2dd43f49f6b3b',
-  };
-
-  next();
-});
-
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use('/', userRoutes);
-app.use('/', cardRoutes);
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Страница не найдена!' });
-});
+const app = express();
 
 app.listen(PORT, (error) => {
   if (error) {
@@ -34,3 +16,18 @@ app.listen(PORT, (error) => {
     console.log(`listening port ${PORT}`);
   }
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62dd54d4aab2dd43f49f6b3b',
+  };
+
+  next();
+});
+app.use((req, res) => {
+  res.status(404).send({ message: 'Страница не найдена!' });
+});
+app.use('/', userRoutes);
+app.use('/', cardRoutes);
