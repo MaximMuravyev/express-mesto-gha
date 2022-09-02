58ff9bb0cfae0b3ccc8e498647daad5fe888f40a
Maxim Muravyev <y89108850235@yandex.ru>
2022-09-02 13:32:41 +0300

    PW14

--- app.js
+++ app.js
@@ -10,20 +10,22 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000 } = process.env;
-mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
-
-app.use(auth);
 
 app.use(bodyParser.json());
 
 app.use('/', authRouter);
-app.use('/', cardRouter);
+
+app.use(auth);
+
 app.use('/', userRouter);
+app.use('/', cardRouter);
 
 app.use((req, res, next) => next(new ErrorNotFound('Неправильный маршрут')));
 app.use(errors());
 app.use(errorHandler);
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT);
