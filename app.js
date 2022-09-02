aadcbbb07463aafeba88dec919f01bf24759f25f
Maxim Muravyev <y89108850235@yandex.ru>
2022-09-02 10:30:35 +0300

    PW14

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
 const cardRouter = require('./routes/cards');
 const userRouter = require('./routes/users');
 const authRoutes = require('./routes/auth');
 const errorRoutes = require('./routes/error');
+
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000 } = process.env;
-mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
 
-app.use('/', authRoutes);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use('/', authRoutes);
+
 app.use('/', errorRoutes);
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
